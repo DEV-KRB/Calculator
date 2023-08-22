772c674d4b439aaf82aca34fe57059dc08c1708d
Kavya Bansore <kavya.bansore25@gmail.com>
2023-08-22 23:47:40 +0530

    final changes

--- src/Application.java
+++ src/Application.java
@@ -1,62 +1,62 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+class Application extends WindowAdapter implements ActionListener{
+    Frame f;
+    Label l1;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
+    Button badd,bsub,bmult,bdiv,bmod,bcalc,bclr,bpts,bneg,bback;
+    double xd;
+    double num1,num2,check;
 
-public class Application extends WindowAdapter implements ActionListener {
+    Application(){
+        f= new Frame("MY CALCULATOR");
+// INSTANTIATING COMPONENETS
+        l1=new Label();
+        l1.setBackground(Color.LIGHT_GRAY);
+        l1.setBounds(50,50,260,60);
 
-    Frame frame;
-    Label label;
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20;
 
-    public Application() {
+        b1=new Button("1");
+        b1.setBounds(50,340,50,50);
+        b2=new Button("2");
+        b2.setBounds(120,340,50,50);
+        b3=new Button("3");
+        b3.setBounds(190,340,50,50);
+        b4=new Button("4");
+        b4.setBounds(50,270,50,50);
+        b5=new Button("5");
+        b5.setBounds(120,270,50,50);
+        b6=new Button("6");
+        b6.setBounds(190,270,50,50);
+        b7=new Button("7");
+        b7.setBounds(50,200,50,50);
+        b8=new Button("8");
+        b8.setBounds(120,200,50,50);
+        b9=new Button("9");
+        b9.setBounds(190,200,50,50);
+        b0=new Button("0");
+        b0.setBounds(120,410,50,50);
+        bneg=new Button("+/-");
+        bneg.setBounds(50,410,50,50);
+        bpts=new Button(".");
+        bpts.setBounds(190,410,50,50);
+        bback=new Button("back");
+        bback.setBounds(120,130,50,50);
 
-        frame = new Frame("My Calculator");
-        label = new Label();
-        label.setBackground(Color.GRAY);
-        label.setBounds(50,50,260,60);
-
-        b1 = new Button("0");
-        b1.setBounds(120, 410, 50, 50);
-        b2 = new Button("1");
-        b2.setBounds(50, 340, 50, 50);
-        b3 = new Button("2");
-        b3.setBounds(120, 340, 50, 50);
-        b4 = new Button("3");
-        b4.setBounds(190, 340, 50, 50);
-        b5 = new Button("4");
-        b5.setBounds(50, 270, 50, 50);
-        b6 = new Button("5");
-        b6.setBounds(120, 270, 50, 50);
-        b7 = new Button("6");
-        b7.setBounds(190, 270, 50, 50);
-        b8 = new Button("7");
-        b8.setBounds(50, 200, 50, 50);
-        b9 = new Button("8");
-        b9.setBounds(120, 200, 50, 50);
-        b10 = new Button("9");
-        b10.setBounds(190, 200, 50, 50);
-        b11 = new Button("+/-");
-        b11.setBounds(50, 410, 50, 50);
-        b12 = new Button(".");
-        b12.setBounds(190, 410, 50, 50);
-        b13 = new Button("=");
-        b13.setBounds(260, 410, 50, 50);
-        b14 = new Button("+");
-        b14.setBounds(260, 340, 50, 50);
-        b15 = new Button("-");
-        b15.setBounds(260, 270, 50, 50);
-        b16 = new Button("*");
-        b16.setBounds(260, 200, 50, 50);
-        b17 = new Button("/");
-        b17.setBounds(260, 130, 50, 50);
-        b18 = new Button("%");
-        b18.setBounds(190, 130, 50, 50);
-        b19 = new Button("back");
-        b19.setBounds(120, 130, 50, 50);
-        b20 = new Button("CE");
-        b20.setBounds(50, 130, 50, 50);
+        badd=new Button("+");
+        badd.setBounds(260,340,50,50);
+        bsub=new Button("-");
+        bsub.setBounds(260,270,50,50);
+        bmult=new Button("*");
+        bmult.setBounds(260,200,50,50);
+        bdiv=new Button("/");
+        bdiv.setBounds(260,130,50,50);
+        bmod=new Button("%");
+        bmod.setBounds(190,130,50,50);
+        bcalc=new Button("=");
+        bcalc.setBounds(245,410,65,50);
+        bclr=new Button("CE");
+        bclr.setBounds(50,130,65,50);
 
 
         b1.addActionListener(this);
@@ -68,139 +68,199 @@ public class Application extends WindowAdapter implements ActionListener {
         b7.addActionListener(this);
         b8.addActionListener(this);
         b9.addActionListener(this);
-        b10.addActionListener(this);
-        b12.addActionListener(this);
-        b11.addActionListener(this);
-        b19.addActionListener(this);
-        b14.addActionListener(this);
-        b15.addActionListener(this);
-        b16.addActionListener(this);
-        b17.addActionListener(this);
-        b18.addActionListener(this);
-        b13.addActionListener(this);
-        b20.addActionListener(this);
-        b11.addActionListener(this);
-        b13.addActionListener(this);
-        b14.addActionListener(this);
-        b15.addActionListener(this);
-        b16.addActionListener(this);
-        b17.addActionListener(this);
-        b18.addActionListener(this);
-        b19.addActionListener(this);
-        b20.addActionListener(this);
+        b0.addActionListener(this);
 
-        frame.add(label);
-        frame.add(b1);
-        frame.add(b2);
-        frame.add(b3);
-        frame.add(b4);
-        frame.add(b5);
-        frame.add(b6);
-        frame.add(b7);
-        frame.add(b8);
-        frame.add(b9);
-        frame.add(b10);
-        frame.add(b14);
-        frame.add(b15);
-        frame.add(b16);
-        frame.add(b17);
-        frame.add(b13);
-        frame.add(b18);
-        frame.add(b20);
-        frame.add(b19);
-        frame.add(b11);
-        frame.add(b12);
+        bpts.addActionListener(this);
+        bneg.addActionListener(this);
+        bback.addActionListener(this);
 
-        frame.addWindowListener(this);
+        badd.addActionListener(this);
+        bsub.addActionListener(this);
+        bmult.addActionListener(this);
+        bdiv.addActionListener(this);
+        bmod.addActionListener(this);
+        bcalc.addActionListener(this);
+        bclr.addActionListener(this);
 
-        frame.setSize(360, 500);
-        frame.setVisible(true);
-        frame.setLayout(null);
+        f.addWindowListener(this);
+//ADDING TO FRAME
+        f.add(l1);
+        f.add(b1); f.add(b2); f.add(b3); f.add(b4); f.add(b5);f.add(b6); f.add(b7); f.add(b8);f.add(b9);f.add(b0);
+
+        f.add(badd); f.add(bsub); f.add(bmod); f.add(bmult); f.add(bdiv); f.add(bmod);f.add(bcalc);
+
+        f.add(bclr); f.add(bpts);f.add(bneg); f.add(bback);
+
+        f.setSize(360,500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
-
+    //FOR CLOSING THE WINDOW
     public void windowClosing(WindowEvent e) {
-        frame.dispose();
+        f.dispose();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e){
         String z,zt;
-
+        //NUMBER BUTTON
         if(e.getSource()==b1){
-            zt=label.getText();
+            zt=l1.getText();
             z=zt+"1";
-            label.setText(z);
+            l1.setText(z);
         }
-
         if(e.getSource()==b2){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"2";
+            l1.setText(z);
         }
-
         if(e.getSource()==b3){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"3";
+            l1.setText(z);
         }
-
         if(e.getSource()==b4){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"4";
+            l1.setText(z);
         }
-
         if(e.getSource()==b5){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"5";
+            l1.setText(z);
         }
-
         if(e.getSource()==b6){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"6";
+            l1.setText(z);
         }
-
         if(e.getSource()==b7){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"7";
+            l1.setText(z);
         }
-
         if(e.getSource()==b8){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"8";
+            l1.setText(z);
         }
-
         if(e.getSource()==b9){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
+            zt=l1.getText();
+            z=zt+"9";
+            l1.setText(z);
+        }
+        if(e.getSource()==b0){
+            zt=l1.getText();
+            z=zt+"0";
+            l1.setText(z);
         }
 
-        if(e.getSource()==b10){
-            zt=label.getText();
-            z=zt+"1";
-            label.setText(z);
-        }
-
-        if(e.getSource()==b12){  //ADD DECIMAL PTS
-            zt=label.getText();
+        if(e.getSource()==bpts){  //ADD DECIMAL PTS
+            zt=l1.getText();
             z=zt+".";
-            label.setText(z);
+            l1.setText(z);
         }
-        if(e.getSource()==b15){ //FOR NEGATIVE
-            zt=label.getText();
+        if(e.getSource()==bneg){ //FOR NEGATIVE
+            zt=l1.getText();
             z="-"+zt;
-            label.setText(z);
+            l1.setText(z);
+        }
+
+        if(e.getSource()==bback){  // FOR  BACKSPACE
+            zt=l1.getText();
+            try{
+                z=zt.substring(0, zt.length()-1);
+            }catch(StringIndexOutOfBoundsException f){return;}
+            l1.setText(z);
+        }
+        //AIRTHMETIC BUTTON
+        if(e.getSource()==badd){                     //FOR ADDITION
+            try{
+                num1=Double.parseDouble(l1.getText());
+            }catch(NumberFormatException f){
+                l1.setText("Invalid Format");
+                return;
+            }
+            z="";
+            l1.setText(z);
+            check=1;
+        }
+        if(e.getSource()==bsub){                    //FOR SUBTRACTION
+            try{
+                num1=Double.parseDouble(l1.getText());
+            }catch(NumberFormatException f){
+                l1.setText("Invalid Format");
+                return;
+            }
+            z="";
+            l1.setText(z);
+            check=2;
+        }
+        if(e.getSource()==bmult){                   //FOR MULTIPLICATION
+            try{
+                num1=Double.parseDouble(l1.getText());
+            }catch(NumberFormatException f){
+                l1.setText("Invalid Format");
+                return;
+            }
+            z="";
+            l1.setText(z);
+            check=3;
+        }
+        if(e.getSource()==bdiv){                   //FOR DIVISION
+            try{
+                num1=Double.parseDouble(l1.getText());
+            }catch(NumberFormatException f){
+                l1.setText("Invalid Format");
+                return;
+            }
+            z="";
+            l1.setText(z);
+            check=4;
+        }
+        if(e.getSource()==bmod){                  //FOR MOD/REMAINDER
+            try{
+                num1=Double.parseDouble(l1.getText());
+            }catch(NumberFormatException f){
+                l1.setText("Invalid Format");
+                return;
+            }
+            z="";
+            l1.setText(z);
+            check=5;
+        }
+        //RESULT BUTTON
+        if(e.getSource()==bcalc){
+            try{
+                num2=Double.parseDouble(l1.getText());
+            }catch(Exception f){
+                l1.setText("ENTER NUMBER FIRST ");
+                return;
+            }
+            if(check==1)
+                xd =num1+num2;
+            if(check==2)
+                xd =num1-num2;
+            if(check==3)
+                xd =num1*num2;
+            if(check==4)
+                xd =num1/num2;
+            if(check==5)
+                xd =num1%num2;
+            l1.setText(String.valueOf(xd));
+        }
+        //FOR CLEARING THE LABEL and Memory
+        if(e.getSource()==bclr){
+            num1=0;
+            num2=0;
+            check=0;
+            xd=0;
+            z="";
+            l1.setText(z);
         }
 
     }
-
-    public static void main(String[]args){
-        Application application = new Application();
+    //MAIN METHOD where objects of MyCalc is instantaiated
+    public static void main(String args[]){
+        Application app = new Application();
     }
 }
-
